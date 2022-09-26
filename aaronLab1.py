56be731201aebcbf8e5ad5fa3afd53026ed0a079
AaronRusk <114408322+AaronRusk@users.noreply.github.com>
2022-09-26 15:20:55 +0100

    Add files via upload

---
+++ aaronLab1.py
@@ -0,0 +1,18 @@
+print('Hello, Aaron')
+
+# Task 3:
+celsius = 56
+
+Faren = celsius * 1.8 + 32
+
+print(celsius, "Celsius is =", Faren, "Farenheit")
+
+# Task 4
+
+print(48426 / (1014-162))
+
+# Task 5
+
+print(113 // 24)
+print(175 // 24)
+print(4+7)
